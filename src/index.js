90671f8cd38b4aeef0312061dba98542ff59736e
Daniel Singer <zigius@gmail.com>
2019-06-25 13:37:19 +0300

    Revert "fuckup".

--- src/index.js
+++ src/index.js
@@ -6,19 +6,18 @@ const Router        = require('queue-router').Router;
 const mysqlClient   = require('./clients/mysql');
 const mailClient    = require('./clients/mail');
 
+const handler = async (content) => {
+    await mysqlClient.saveDailyQuote(content.to, content.dailyQuote);
+    await mailClient.send(content.to, content.dailyQuote);
+    console.log('message_processed');
+};
 
 async function start() {
     const router = new Router();
 
-    router.add('SEND_DAILY_QUOTE', {
-        handler: async (content) => {
-            await mysqlClient.saveDailyQuote(content.to, content.dailyQuote);
-            await mailClient.send(content.to, content.dailyQuote);
-            console.log('message_processed');
-        }
-    });
+    router.add('SEND_DAILY_QUOTE', { handler });
 
-
+    // uses aws-sdk receiveMessage under the hood
     workerFactory.create('SQS', router, config.sqs)
         .on('error', error => console.error('worker#error', error))
         .init()
